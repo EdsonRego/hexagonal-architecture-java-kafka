8f6433711488f69cc56bb43b2fafb8c6e47d39e7
EdsonRego <edsonxrego@gmail.com>
2025-11-02 14:28:13 -0300

    Clien implementation

--- src/main/java/com/edsonrego/hexagonal/HexagonalApplication.java
+++ src/main/java/com/edsonrego/hexagonal/HexagonalApplication.java
@@ -2,7 +2,9 @@ package com.edsonrego.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HexagonalApplication {
 
