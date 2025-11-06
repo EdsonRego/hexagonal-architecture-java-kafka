faf72201247392af4e5e42b73ae23b9be300b877
EdsonRego <edsonxrego@gmail.com>
2025-11-05 21:29:11 -0300

    Customer Controller

--- src/main/java/com/edsonrego/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
+++ src/main/java/com/edsonrego/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -1,9 +1,10 @@
 package com.edsonrego.hexagonal.application.core.usecase;
 
 import com.edsonrego.hexagonal.application.core.domain.Customer;
+import com.edsonrego.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.edsonrego.hexagonal.application.ports.out.FindCustomerByIdOutPutPort;
 
-public class FindCustomerByIdUseCase {
+public class FindCustomerByIdUseCase implements FindCustomerByIdInputPort {
 
     private  final FindCustomerByIdOutPutPort findCustomerByIdOutPutPort;
 
@@ -11,6 +12,7 @@ public class FindCustomerByIdUseCase {
         this.findCustomerByIdOutPutPort = findCustomerByIdOutPutPort;
     }
 
+    @Override
     public Customer find(String id){
         return findCustomerByIdOutPutPort.find(id)
                 .orElseThrow(()->new RuntimeException("Customer not found"));
