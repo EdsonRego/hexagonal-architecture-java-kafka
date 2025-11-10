f79c679b70d13536d048d5e8f504ad99935cb960
EdsonRego <edsonxrego@gmail.com>
2025-11-10 10:26:55 -0300

    Customer Updade - controller

--- src/main/java/com/edsonrego/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/edsonrego/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,8 +3,10 @@ package com.edsonrego.hexagonal.adapters.in.controller;
 import com.edsonrego.hexagonal.adapters.in.controller.mapper.CustomerMapper;
 import com.edsonrego.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.edsonrego.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.edsonrego.hexagonal.application.core.domain.Customer;
 import com.edsonrego.hexagonal.application.ports.in.FindCustomerByIdInputPort;
 import com.edsonrego.hexagonal.application.ports.in.InsertCustomerInputPort;
+import com.edsonrego.hexagonal.application.ports.in.UpdateCustomerInputPort;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,9 @@ public class CustomerController {
     private FindCustomerByIdInputPort findCustomerByIdInputPort;
 
     @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
     private CustomerMapper customerMapper;
 
     @PostMapping
@@ -35,5 +40,13 @@ public class CustomerController {
         var customer = findCustomerByIdInputPort.find(id);
         var CustomerResponse = customerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok().body(CustomerResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id, @Valid @RequestBody CustomerRequest customerRequest){
+        Customer customer = customerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        updateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
     }
 }
